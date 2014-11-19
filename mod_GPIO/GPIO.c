4ee39725cac052c97e277fb53eeb7884bfdde446
WALLE <walle@beagle.bone>
2014-11-19 15:58:28 +0000

    Updated GPIO after testing

--- mod_GPIO/GPIO.c
+++ mod_GPIO/GPIO.c
@@ -3,6 +3,47 @@
 #include <stdlib.h>
 #include <BBBio_lib/BBBiolib.h>
 
+/* Main for testing? */
+int main(int argc, char** argv)
+{
+  int test;
+  printf("GPIO Module Testing\n\n");
+  if(argc < 3)
+  {
+    printf("Usage: %s (1|2|3) value\n", argv[0]);
+    printf("\t 1 - Battery level \n");
+    printf("\t 2 - Turning servo angle \n");
+    printf("\t 3 - Motor speed value\n");
+    return -1;
+  }
+  
+  test = atoi(argv[1]);
+
+  printf("Initializing IO lib\n");
+  initialize();
+
+  if(test == 1)
+  {
+    printf("Battery level at %s\n",argv[2]);
+    setBatteryLEDs(atoi(argv[2]));
+  }else if(test == 2)
+  {
+    printf("Tuning servo 1 to %s\n",argv[2]);
+    setTurn(atoi(argv[2]));
+  }else if(test == 3)
+  {
+    enableHBridge();
+    printf("Motor speed to %s \n",argv[2]);
+    setSpeed(atoi(argv[2]));
+  }else{
+    printf("Invalid value\n");
+  }
+
+  printf("Cleanup\n");
+  deinitialize();
+  return 0;
+}
+
 /* initialize - Starts up the configuration for GPIO
  * include pins and PWM
  */
@@ -10,10 +51,9 @@ int initialize(void)
 {
   // Initialize library
   iolib_init();
-  
   // Set Batteries as output pins
 	iolib_setdir(8, LED_BAT1, BBBIO_DIR_OUT);
-	iolib_setdir(8, LED_BAT2, BBBIO_DIR_OUT);
+  iolib_setdir(8, LED_BAT2, BBBIO_DIR_OUT);
 	iolib_setdir(8, LED_BAT3, BBBIO_DIR_OUT);
 	iolib_setdir(8, LED_BAT4, BBBIO_DIR_OUT);
 
@@ -26,9 +66,9 @@ int initialize(void)
 	BBBIO_PWMSS_Setting(MOTOR_A_PWM,  MOTOR_FREQ,   0.0,  0.0);   // Clear settings
 	BBBIO_PWMSS_Setting(MOTOR_B_PWM,  MOTOR_FREQ,   0.0,  0.0);   // Clear settings
 
-	BBBIO_ehrPWM_Enable(SERVO_PWM);   // Servo channel
-	BBBIO_ehrPWM_Enable(MOTOR_A_PWM); // Motor A channel
-	BBBIO_ehrPWM_Enable(MOTOR_B_PWM); // Motor B channel
+	//BBBIO_ehrPWM_Enable(SERVO_PWM);   // Servo channel
+	//BBBIO_ehrPWM_Enable(MOTOR_A_PWM); // Motor A channel
+	//BBBIO_ehrPWM_Enable(MOTOR_B_PWM); // Motor B channel
 
   return 1;
 }
@@ -39,13 +79,17 @@ int deinitialize(void)
   // H-Bridge Enable/Disable
   pin_low(8, HBRIDGE_EN);
 
+  iolib_free();
+  return 0;
+}
+
+int disablePWM(void)
+{
   // Disable PWM channels
 	BBBIO_ehrPWM_Disable(SERVO_PWM);   // Servo channel
 	BBBIO_ehrPWM_Disable(MOTOR_A_PWM); // Motor A channel
 	BBBIO_ehrPWM_Disable(MOTOR_B_PWM); // Motor B channel
-
-  iolib_free();
-  return 0;
+  return 1;
 }
 
 /* Enable the H-Bridge on the motor board */
@@ -65,7 +109,7 @@ int disableHBridge(void)
 /* setBattery - sets battery level by 4 LEDs on motor-board
  * Param: percent (uint8 from 0 to 100)
  */
-int setBatteryLEDs(uint8 percent)
+int setBatteryLEDs(unsigned int percent)
 {
   // Reset to clear
   pin_low(8,LED_BAT1);
@@ -87,31 +131,35 @@ int setBatteryLEDs(uint8 percent)
 }
 
 // Both servos need to be stored so we don't mess one up
-int setTurn(int16 angle)
+int setTurn(signed int angle)
 {
-	const float duty_wheels = 20.0f;
-	const float duty_camera = 50.0f;
+	float duty_wheels = (float)angle;
+	float duty_camera = 50.0f;
 
 	BBBIO_PWMSS_Setting(SERVO_PWM, SERVO_FREQ, duty_wheels, duty_camera);
   return 1;
 }
 
 // Both servos need to be stored so we don't mess one up
-int setCamera(int16 angle)
+int setCamera(signed int angle)
 {
-	const float duty_wheels = 20.0f;
-	const float duty_camera = 50.0f;
+	const float duty_wheels = 10.0f;
+	const float duty_camera = angle/100;
   
 	BBBIO_PWMSS_Setting(SERVO_PWM, SERVO_FREQ, duty_wheels, duty_camera);
   return 1;
 }
 
 //TODO: differential turning based on stored angle for turn servo
-int setSpeed(uint8 speed)
+int setSpeed(unsigned int speed)
 {
-	float duty = speed/100;
+	float duty = (float)speed;
   
-	BBBIO_PWMSS_Setting(MOTOR_A_PWM, MOTOR_FREQ, duty, 0.0);
-	BBBIO_PWMSS_Setting(MOTOR_B_PWM, MOTOR_FREQ, duty, 0.0);
+	BBBIO_PWMSS_Setting(MOTOR_A_PWM, MOTOR_FREQ, duty, duty);
+	BBBIO_PWMSS_Setting(MOTOR_B_PWM, MOTOR_FREQ, duty, duty);
+
+  BBBIO_ehrPWM_Enable(MOTOR_A_PWM);
+  BBBIO_ehrPWM_Enable(MOTOR_B_PWM);
+  printf("%0.2f percent of motor\n",duty);
   return 1;
 }
--- mod_GPIO/GPIO.h
+++ mod_GPIO/GPIO.h
@@ -14,12 +14,12 @@ extern "C" {
 #define HBRIDGE_EN  11    // P8
 #define BAT_SENSE   AIN1  // P9
 
-#define SERVO_PWM       BBBIO_PWMSS2 // Channel A = turn, Channel B = Camera
-#define SERVO_FREQ      50           // 50 Hz
+#define SERVO_PWM       BBBIO_PWMSS2  // Channel A = turn, Channel B = Camera
+#define SERVO_FREQ      50.0f           // 50 Hz
 
-#define MOTOR_A_PWM     BBBIO_PWMSS0 // Both channels A,B
-#define MOTOR_B_PWM     BBBIO_PWMSS1 // Both channels A,B
-#define MOTOR_FREQ      20000        // 20Khz
+#define MOTOR_A_PWM     BBBIO_PWMSS0  // Both channels A,B
+#define MOTOR_B_PWM     BBBIO_PWMSS1  // Both channels A,B
+#define MOTOR_FREQ      50.0f        // 20Khz
 
 /* initialize - Starts up the configuration for GPIO
  * include pins and PWM
@@ -38,16 +38,16 @@ int disableHBridge(void);
 /* setBattery - sets battery level by 4 LEDs on motor-board
  * Param: percent (uint8 from 0 to 100)
  */
-int setBatteryLEDs(uint8 percent);
+int setBatteryLEDs(unsigned int percent);
 
 // Both servos need to be stored so we don't mess one up
-int setTurn(int16 angle);
+int setTurn(signed int angle);
 
 // Both servos need to be stored so we don't mess one up
-int setCamera(int16 angle);
+int setCamera(signed int angle);
 
 //TODO: differential turning based on stored angle for turn servo
-int setSpeed(uint8 speed);
+int setSpeed(unsigned int speed);
 
 #ifdef __cplusplus
   }
